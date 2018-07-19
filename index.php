6cf9f5cdbd43565e66afc29a9e09641a5fb62956
tcheuD <tcheutcheud@gmail.com>
2018-07-19 16:49:11 +0200

    Added index.php in the root directory

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-require('controller/frontend.php');
+session_start();
 
+define('MM_BRUTE_FILE', './var/cache/forbiddenIp.php');
+define('MM_BRUTE_WINDOW', 15*60);
+define('MM_BRUTE_ATTEMPTS', 5);
+
+if(isset($_SESSION["nickname"])){
+    echo "connecté en tant que " .$_SESSION["nickname"]."<br/>";
+    echo "email : " .$_SESSION["email"]."<br/><br/>";
+}
+
+require __DIR__.'./config/Router.php';
+handleRequest($_SERVER);
