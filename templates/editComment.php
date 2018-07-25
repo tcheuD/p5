b828d788ac6c12c1c90eb434b05fbfc1ed979a41
tcheuD <tcheutcheud@gmail.com>
2018-07-25 18:42:13 +0200

    Added template.php in the template directory.

--- templates/editComment.php
+++
@@ -1,8 +0,0 @@
-<form action="/p5/editComment/<?=$id?>" method="post">
-    <p>
-        <label>modifier le commentaire :</label><br>
-        <textarea name="comment" rows="7" cols="50"><?=$content?></textarea><br />
-
-        <input type="submit" value="modifier" />
-    </p>
-</form>
