3836cc7d22ce561b6e01654c6acb4e9540d697f8
tcheuD <tcheutcheud@gmail.com>
2018-09-17 15:10:38 +0200

    Build : minor changes in order to implement ParameterBag.php and Request.php

--- etc/Router.php
+++ etc/Router.php
@@ -2,14 +2,18 @@
 
 namespace Core;
 
+use Core\Interfaces\RequestInterface;
+
 class Router
 {
 
     private $routes = [];
+    private $actionResolver;
 
     public function __construct()
     {
         $this->constructRoute();
+        $this->actionResolver = new ActionResolver();
     }
 
     public function constructRoute()
@@ -18,25 +22,24 @@ class Router
         foreach ($routes as $key => $value) {
             $route [] = new Route(
                 $value['path'],
-                $value['action']
+                $value['action'],
+                $value['params'] ?? [],
+                $_SERVER['REQUEST_URI'],
+                $value["methods"]
             );
         }
         $this->routes= $route;
     }
 
-    public function catchParams(Route $route, $uri)
-    {
-        $route->match($uri);
-    }
 
-    public function handleRequest()
+    public function handleRequest(RequestInterface $request)
     {
+
         foreach ($this->routes as $route) {
-            $this->catchParams($route, $_SERVER['REQUEST_URI']);
-            switch ($_SERVER['REQUEST_URI']) {
+            switch ($request->getRequestUri()) {
+                //case $route->getPath() && \in_array($request->getMethod(), $route->getMethods()):
                 case $route->getPath():
-                    $actionResolver = new ActionResolver();
-                    $actionResolver->resolveAction($route->getAction(), $route->getParam());
+                    return $this->actionResolver->resolveAction($route->getAction(), $route->getParam());
                     break;
             }
         }
