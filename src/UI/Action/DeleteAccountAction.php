e64034afb4446408f0de43c96d5a7d8dd3d528d5
tcheuD <tcheutcheud@gmail.com>
2018-10-30 16:15:20 +0100

    build: action has been adapted in order to work with twig and a new Response class

--- src/UI/Action/DeleteAccountAction.php
+++ src/UI/Action/DeleteAccountAction.php
@@ -3,32 +3,59 @@
 namespace App\UI\Action;
 
 use App\Domain\Repository\AccountRepository;
+use App\Domain\Repository\CommentRepository;
+use App\Domain\Repository\PostRepository;
 use App\UI\Action\Interfaces\DeleteAccountActionInterface;
-
-require_once __DIR__ . './../../../etc/viewLoader.php';
-require_once __DIR__.'/Interfaces/DeleteAccountActionInterface.php';
+use Core\Response;
+use Core\Twig;
 
 class DeleteAccountAction implements DeleteAccountActionInterface
 {
     private $accountRepository;
+    private $postRepository;
+    private $commentRepository;
+    private $session;
+    private $twig;
 
     public function __construct()
     {
         $this->accountRepository = new AccountRepository();
+        $this->postRepository = new PostRepository();
+        $this->commentRepository = new CommentRepository();
+        $this->twig = new Twig();
     }
 
-    public function __invoke($id, array $request = []) {
+    public function __invoke($request, $id) {
+
+        $this->session = $request->getSession();
+
         $showForm = false;
-        if (isset($_SESSION['id'])) {
-            if ($id == intval($_SESSION['id']) || CheckUserGroupAction::checkUserGroup()) {
+
+        if ($this->session->parameterExist('id')) {
+
+            if ($id == intval($this->session->get('id')) || $this->session->isAdmin()) {
                 $showForm = true;
-                $user = $this->accountRepository->getUser($id);
-                if (isset($_SESSION['id'])) {
-                    $status = $this->accountRepository->deleteAccount($user);
+                $userId = $this->session->get('id');
+
+                if (isset($_POST['password'])) {
+
+                    $query = $this->accountRepository->getUser($userId);
+                    $hashed_password = $query->getPassword();
+                    $password = password_verify($_POST['password'], $hashed_password);
+
+                    if ($password) {
+                        $user = $this->accountRepository->getUser($id);
+                        $this->postRepository->deleteAllFromUser($id);
+                        $this->commentRepository->deleteAllFromUser($id);
+                        $this->accountRepository->deleteAccount($user);
+                        $this->session->destroySession();
+                        header("Location: /p5/");
                 }
             }
         }
-        require loadView('deleteAccount.php');
+            return new Response($this->twig->getTwig($request)->render('deleteAccount.html.twig', array(
+                'showForm' => $showForm,
+                'id' => $id)));
     }
 }
-
+}
