d8e04fddd00cf86d2c9f04fe2deddd808f037ff5
tcheuD <tcheutcheud@gmail.com>
2018-11-27 20:55:37 +0100

    Feat: added links

--- view/frontend/myAccount.php
+++ view/frontend/myAccount.php
@@ -1,15 +1,14 @@
 <?php
-require_once __DIR__ . './../../config/templateLoader.php';
+require_once __DIR__.'./../../etc/templateLoader.php';
 
 $title = "Mon compte" ?>
 
-<?php ob_start();?>
+<?php ob_start(); ?>
 
-<section>
-    <a href=../p5/edit-account/<?=$userId?>>Modifier mes informations</a>
-    <br><br>
-    <?=$posts?>
-</section>
+<a href=/p5/edit-account/<?=$_SESSION['id']?>>Modifier mon compte</a>
+<a href=/p5/mon-compte/articles><?=$postsNumber?> articles en ligne</a>
+<a href=/p5/mon-compte/commentaires><?=$commentsNumber?> commentaires en ligne</a>
+<a href=/p5/delete-account/<?=$_SESSION['id']?>>Supprimer mon compte</a>
 
 <?php $content = ob_get_clean();
 require loadTemplate('template.php');
