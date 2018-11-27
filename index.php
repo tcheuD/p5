e0ad0aedf2216aea529ff3ffc6e1ebda49bed3d7
tcheuD <tcheutcheud@gmail.com>
2018-11-27 20:57:28 +0100

    Refactor: removed old code

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
-
-session_start();
-
 require_once __DIR__.'./vendor/autoload.php';
 
 use App\Kernel;
 use Core\Request;
-use Core\Router;
+use Core\SessionHandler;
 
 define('MM_BRUTE_FILE', './var/cache/forbiddenIp.php');
 define('MM_BRUTE_WINDOW', 15*60);
 define('MM_BRUTE_ATTEMPTS', 5);
 
-if(isset($_SESSION["nickname"])){
-    echo "connecté en tant que " .$_SESSION["nickname"]."<br/>";
-    echo "email : " .$_SESSION["email"]."<br/><br/>";
-}
-
 $request = Request::createFromGlobals();
 $kernel = new Kernel();
-$kernel->handle($request);
+$response = $kernel->handle($request);
+$response->send();
