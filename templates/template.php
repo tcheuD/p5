4755feec7d7af3f671659e611e245b1abf419081
tcheuD <tcheutcheud@gmail.com>
2018-11-28 09:58:47 +0100

    Refactor updated path

--- templates/template.php
+++ templates/template.php
@@ -2,8 +2,24 @@
 <html>
 <head>
     <title><?=$title?></title>
+    <!--<link rel="stylesheet" href=/p5/public/css/style.css>-->
+    <link href="/vendor/twbs/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-<?=$content?>
+
+<header>
+    <nav>
+        <?php include "nav.php"?>
+    </nav>
+<h1><?=$title?></h1>
+
+</header>
+
+<div id='section_wrapper'>
+    <section>
+    <?=$content?>
+    </section>
+</div>
+
 </body>
 </html>
