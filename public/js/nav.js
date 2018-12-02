488d34be71d6a837151a6326bc2051ee5ccf72f6
tcheuD <tcheutcheud@gmail.com>
2018-12-02 15:16:58 +0100

    Style : arranged a css & js template from an html5up

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,3 +1,3 @@
 var pathArray = window.location.pathname.split('/');
-var secondLevelLocation = pathArray[2];
+var secondLevelLocation = pathArray[1];
 $('#' + secondLevelLocation).addClass("current");
