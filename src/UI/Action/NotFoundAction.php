86fcf39367dca55d05b80302487426c9fb95c0f9
tcheuD <tcheutcheud@gmail.com>
2018-12-04 15:04:30 +0100

    Added notFound template

--- src/UI/Action/NotFoundAction.php
+++ src/UI/Action/NotFoundAction.php
@@ -1,14 +1,20 @@
 <?php
 
 namespace App\UI\Action;
-
-
 use Core\Response;
+use Core\Twig;
 
 class NotFoundAction
 {
+    private $twig;
+
+    public function __construct()
+    {
+        $this->twig = new Twig();
+    }
+
     public function __invoke($request)
     {
-        return new Response("hello world");
+        return new Response($this->twig->getTwig($request)->render('notFound.html.twig'));
     }
 }
